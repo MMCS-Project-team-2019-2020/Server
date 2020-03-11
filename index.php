039173dfcc0e4ab3b7b954fb354bd2a733919f13
olader09 <reid09@mail.ru>
2020-03-11 18:27:57 +0300

    коментарии по запросам

--- index.php
+++ index.php
@@ -25,25 +25,31 @@ if (isgive()) {
 
 
 
-
+//Запрос - логинится//Пример:   ?action=login&login=vasya&password=4testtststs
 if ($action == "login") {
 	require("models/login.php");
 }
+//Запрос - регистрация//Пример:   ?action=register&login=vasya&password=4testtststs&name=Василий&phone=89995556677
 if ($action == "register") {
 	require("models/register.php");
 }
+//Запрос - получить профиль(данные акка)//Пример:   ?action=get-profile&login=vasya
 if ($action == "get-profile") {
 	require("models/profile.php");
 }
+//Запрос - получить список визиток владельца//Пример:  ?action=get-list-card&login=vasya&password=123trxzxcsx
 if ($action == "get-list-card") {
 	require("models/listcard.php");
 }
+//Запрос - Создать визитку//Пример:  ?action=card-create&login=login&card-name=testcard&card-caption=ratatatata
 if ($action == "card-create") {
 	require("models/cardcreate.php");
 }
+//Запрос - поделится визиткой//Пример: ?action=give-card&id_owner=1001&id_recipient=1000&id_card=10004 (id - это будет QR код визитки, тоесть шарить будем по id)
 if ($action == "give-card") {
 	require("models/givecard.php");
 }
+//Запрос - получить данные визитки//Пример: ?action=give-card&id_owner=1001&id_card=10004
 if ($action == "get-card") {
 	require("models/getcard.php");
 }
