4115336149ad6774af4d8f1458d3536b69838b2c
olader09 <reid09@mail.ru>
2020-04-05 15:05:46 +0300

    information error connection to db

--- db.php
+++ db.php
@@ -5,9 +5,14 @@ define('MYSQL_PASSWORD', '386419');
 define('MYSQL_DB', 'u328338304_proj');
 
 function db_connect(){//конект с базой
-    $link = mysqli_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB) 
-        or die("Error: ".mysqli_error($link));
-    if(!mysqli_set_charset($link, "utf8")){
-        printf("Error: ".mysqli_error($link));}
+    $link = mysqli_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB);
+       if (!$link) {
+    echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
+    echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+	}
+    if(!mysqli_set_charset($link, "utf8"))
+        printf("Error: ".mysqli_error($link));
     return $link;
 }
